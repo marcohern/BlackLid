bb88bd78e558be7da06ce344e95fec8169fcb002
Marco Hernandez <marcohern@gmail.com>
2015-02-08 11:45:46 -0500

    Initial Commit

---
+++ BlackLid/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  BlackLid
+//
+//  Created by Marco Hernandez on 8/02/15.
+//  Copyright (c) 2015 Marco Hernandez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
