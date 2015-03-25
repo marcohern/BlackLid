3a105abc6390113b076f1d57df4532472da84745
marcohern <marcoh@MacBook-Pro-de-Marco.local>
2015-03-24 23:30:53 -0500

    added Character class

---
+++ BlackLid/objects/Character.cpp
@@ -0,0 +1,23 @@
+//
+//  Character.cpp
+//  BlackLid
+//
+//  Created by Marco Hernandez on 17/03/15.
+//  Copyright (c) 2015 Marco Hernandez. All rights reserved.
+//
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <string>
+
+#include "../input/InputCommand.h"
+#include "../core/Fps.h"
+#include "../core/Engine.h"
+#include "../core/Vector2D.h"
+#include "../core/define.h"
+#include "../core/Settings.h"
+#include "../core/Fps.h"
+#include "../core/Draw.h"
+#include "Placeable.h"
+#include "Drawable.h"
+
+#include "Character.h"
---
+++ BlackLid/objects/Character.h
@@ -0,0 +1,20 @@
+//
+//  Character.h
+//  BlackLid
+//
+//  Created by Marco Hernandez on 17/03/15.
+//  Copyright (c) 2015 Marco Hernandez. All rights reserved.
+//
+
+#ifndef __BlackLid__Character__
+#define __BlackLid__Character__
+
+#include <stdio.h>
+
+namespace objects {
+    class Character : public Drawable {
+        
+    };
+}
+
+#endif /* defined(__BlackLid__Character__) */
